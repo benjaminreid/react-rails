553ba23a87bb16f448420657844a83232013793c
Benjamin Reid <benjamin@nouveller.com>
2017-06-22 16:32:38 +0100

    Setup styled-components

--- app/javascript/components/hello.js
+++ app/javascript/components/hello.js
@@ -1,7 +1,13 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledHello = styled.div`
+  background-color: red;
+  color: white;
+`;
 
 const Hello = ({ name }) =>
-  <div>Hello {name}</div>
+  <StyledHello>Hello, {name}</StyledHello>
 ;
 
 export default Hello;
